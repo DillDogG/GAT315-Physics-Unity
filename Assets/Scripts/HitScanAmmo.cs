26b7afe6fc762a45b9e322becf126b8a46bf8cb6
DillDogG <dilldoggaming@gmail.com>
2024-04-11 11:59:13 -0600

    update

---
+++ Assets/Scripts/HitScanAmmo.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HitScanAmmo : MonoBehaviour
+{
+    //[SerializeField] 
+    //void Start()
+    //{
+    //    if (Physics.Raycast(transform.position, transform.forward, out RaycastHit raycastHit, distance, layerMask))
+    //    {
+    //        if (hitPrefab != null)
+    //        {
+    //            Instantiate(hitPrefab, raycastHit.point, Quaternion.LookRotation(raycastHit.normal));
+    //        }
+    //    }
+
+    //    Destroy(gameObject);
+    //}
+
+
+}
--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -1,29 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class Weapon : MonoBehaviour
 {
     [SerializeField] GameObject ammo;
     [SerializeField] Transform emission;
-    [SerializeField] float fireRate;
+    [SerializeField] AudioSource audioSource;
 
-    bool fireReady = true;
-
+    public bool equipped = false;
     void Update()
     {
-        if (fireReady && Input.GetMouseButtonDown(0))
-        {
-            Instantiate(ammo, emission.position, emission.rotation);
-            fireReady = false;
-            StartCoroutine(FireTimer(fireRate));
-        }
-    }
+        Debug.DrawRay(emission.position, emission.forward * 10, Color.red);
 
-    IEnumerator FireTimer(float time)
-    {
-        yield return new WaitForSeconds(time);
-        fireReady = true;
+        if (equipped && Input.GetMouseButtonDown(0))
+        {
+            if (audioSource != null) audioSource.Play();
+            Instantiate(ammo, emission.position, emission.rotation);
+        }
     }
 }
